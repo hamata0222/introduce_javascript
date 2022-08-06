146a4a0279cee394cc477e93054db4f04d56e307
hamata0222 <jp.ne.hiroshi@gmail.com>
2022-08-06 13:10:40 +0900

    テンプレート文字列と配列のサンプルを追加

---
+++ Chapter02/array.js
@@ -0,0 +1,3 @@
+var arr = ["JavaScript", ["JQuery", "Ajax"], "ASP.NET"];
+
+console.log(arr[1][0]);
---
+++ Chapter02/template.js
@@ -0,0 +1,6 @@
+let user_name = "Hiroshi";
+let template_str = `Hello ${user_name}.
+Welcome to my web site!`;
+
+// console.log(template_str);
+window.alert(template_str);
